474f253af0a9e62cb1e166792d250b3bb7ba9ac6
Patrick Meade <blinkdog@protonmail.com>
2020-05-17 13:40:50 -0500

    Added SQL to remove nanotrasen_relation column from table characters

--- SQL/scorpio/pr00011.sql
+++ SQL/scorpio/pr00011.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `characters` ADD `ark_soft_relation` varchar(45) NOT NULL;
 UPDATE `characters` SET `ark_soft_relation` = `nanotrasen_relation`;
+ALTER TABLE `characters` DROP COLUMN `nanotrasen_relation`;
