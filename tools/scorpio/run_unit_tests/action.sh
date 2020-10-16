0f2bc3b15e3d192b1fe52061af6891fdbbe30edc
Patrick Meade <blinkdog@protonmail.com>
2020-10-16 00:23:13 -0500

    Third time is a charm

--- tools/scorpio/run_unit_tests/action.sh
+++ tools/scorpio/run_unit_tests/action.sh
@@ -2,4 +2,8 @@
 # run_unit_tests.sh
 # Prepare a build to run unit tests
 
+# add the flag to trigger unit tests
+echo "#define TRAVISBUILDING" >> ci-scorpio.dme
+echo paradise.dme >> ci-scorpio.dme
+
 cp config/example/dbconfig.txt config/dbconfig.txt
