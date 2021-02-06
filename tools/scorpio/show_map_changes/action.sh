2362a3a82ea22eb4dbaea4c523ba87fe603a6452
Patrick Meade <blinkdog@protonmail.com>
2021-02-06 13:47:53 -0600

    Need history and relative paths

--- tools/scorpio/show_map_changes/action.sh
+++ tools/scorpio/show_map_changes/action.sh
@@ -3,18 +3,24 @@
 # Generate images to highlight map changes, if any
 
 # what does the file system look like inside CI?
+echo "pwd"
 pwd
+echo "ls -l"
 ls -l
 
 # what does git look like from inside CI?
+echo "git status"
 git status
+echo "git log"
 git log --pretty=oneline | head -15
 
 # let's see what's changed in our PR branch...
+echo "git diff master"
 git diff master --name-only
 
 # let's generate a map for Emerald...
+echo "generate image using Docker"
 docker pull scorpiostation/spacemandmm:latest
 docker create --name delete_me scorpiostation/spacemandmm:latest
 docker cp delete_me:/spacemandmm/target/release/dmm-tools dmm-tools
-./dmm-tools minimap /home/pmeade/projects/scorpio-sidecart/_maps/map_files/emerald/emerald.dmm
+./dmm-tools minimap _maps/map_files/emerald/emerald.dmm
