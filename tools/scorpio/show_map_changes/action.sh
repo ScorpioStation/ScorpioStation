93aee40c6b01a9b271ef13d8335dc64af793d09e
Patrick Meade <blinkdog@protonmail.com>
2021-02-13 03:39:38 -0600

    Compute ARTIFACT_TAG before fetching master

--- tools/scorpio/show_map_changes/action.sh
+++ tools/scorpio/show_map_changes/action.sh
@@ -5,6 +5,8 @@
 # DEBUG: output a few things that might be useful for debugging
 echo "git show-ref"
 git show-ref
+ARTIFACT_TAG=$(git show-ref | head -1 | awk -- '{print $2}' | sha256sum | awk -- '{print $1}' | cut -c 1-12)
+echo "ARTIFACT_TAG: ${ARTIFACT_TAG}"
 
 # obtain what the maps look like at origin/master
 git fetch --depth=1 --no-auto-gc --no-recurse-submodules --prune origin +refs/heads/master:refs/remotes/origin/master
@@ -63,7 +65,6 @@ done
 
 # compute an artifact tag
 # ARTIFACT_TAG = the first 12 characters of the sha256sum of the PR ref (e.g. "refs/remotes/pull/348/merge")
-ARTIFACT_TAG=$(git show-ref | head -1 | awk -- '{print $2}' | sha256sum | awk -- '{print $1}' | cut -c 1-12)
 echo "ARTIFACT_TAG=${ARTIFACT_TAG}" >> $GITHUB_ENV
 
 # view what terrible carnage we have wrought
